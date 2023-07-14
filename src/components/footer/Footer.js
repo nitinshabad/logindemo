375f8bb83b0c3ef4567ecd15442231e1b85c8c42
Adarsh <ad0071203@gmail.com>
2023-07-15 00:22:44 +0530

    basic details

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,7 +11,12 @@ function Footer() {
         <div className="wave" id="wave4"></div>
       </div>
       <div className="Address">
-        <p>ajfbb@gmail.com</p>
+        <h3>Reach us out here</h3>
+        <ol>
+          <li>nbierbvk@gmail.com</li>
+          <li>654656161531</li>
+          <li>46, 95 abc, xyz jagah</li>
+        </ol>
         <div className="Map">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28399.14644416308!2d77.97077217431637!3d27.159646999999993!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x397477ee9d89102f%3A0x443a552e860e4e36!2sUnicorn%2C%20Apple%20Premium%20Reseller!5e0!3m2!1sen!2sin!4v1689360174613!5m2!1sen!2sin"
